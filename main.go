71ca553ad4eddcf26babfd088cc4def1ee2b1aa6
John Wass <wassj@ctc.com>
2020-01-31 15:25:08 -0500

    clean up commenting

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"flag"
 	"fmt"
 	tf "github.com/tensorflow/tensorflow/tensorflow/go"
@@ -20,7 +19,6 @@ import (
 
 // Some constants specific to the pre-trained model at:
 // https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
-//
 // - The model was trained after with images scaled to 224x224 pixels.
 // - The colors, represented as R, G, B in 1-byte each were converted to
 //   float using (value - Mean)/Scale.
@@ -45,37 +43,6 @@ type Detect struct {
 }
 
 func main() {
-	// An example for using the TensorFlow Go API for image recognition
-	// using a pre-trained inception model (http://arxiv.org/abs/1512.00567).
-	//
-	// Sample usage: <program> -dir=/tmp/modeldir -image=/path/to/some/jpeg
-	//
-	// The pre-trained model takes input in the form of a 4-dimensional
-	// tensor with shape [ BATCH_SIZE, IMAGE_HEIGHT, IMAGE_WIDTH, 3 ],
-	// where:
-	// - BATCH_SIZE allows for inference of multiple images in one pass through the graph
-	// - IMAGE_HEIGHT is the height of the images on which the model was trained
-	// - IMAGE_WIDTH is the width of the images on which the model was trained
-	// - 3 is the (R, G, B) values of the pixel colors represented as a float.
-	//
-	// And produces as output a vector with shape [ NUM_LABELS ].
-	// output[i] is the probability that the input image was recognized as
-	// having the i-th label.
-	//
-	// A separate file contains a list of string labels corresponding to the
-	// integer indices of the output.
-	//
-	// This example:
-	// - Loads the serialized representation of the pre-trained model into a Graph
-	// - Creates a Session to execute operations on the Graph
-	// - Converts an image file to a Tensor to provide as input to a Session run
-	// - Executes the Session and prints out the label with the highest probability
-	//
-	// To convert an image file to a Tensor suitable for input to the Inception model,
-	// this example:
-	// - Constructs another TensorFlow graph to normalize the image into a
-	//   form suitable for the model (for example, resizing the image)
-	// - Creates and executes a Session to obtain a Tensor in this normalized form.
 	modeldir := flag.String("dir", "", "Directory containing the trained model and labels")
 	imagefile := flag.String("image", "", "Path of a JPEG-image to extract labels for")
 	flag.Parse()
@@ -84,7 +51,7 @@ func main() {
 		return
 	}
 	// Load the serialized GraphDef from a file.
-	modelfile, labelsfile, err := modelFiles(*modeldir, "multires_aug")
+	modelfile, labelsfile, err := modelFiles(*modeldir, "multires")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -130,16 +97,8 @@ func main() {
 			SubImage(r image.Rectangle) image.Image
 		}).SubImage(image.Rect(w, h, w+W, h+H))
 
-		outputFile, err := os.Create(fmt.Sprintf("/tmp/chip-%v.png",i))
-		if err != nil {
-			// Handle error
-		}
-
-		// Encode takes a writer interface and an image interface
-		// We pass it the File and the RGBA
+		outputFile, _ := os.Create(fmt.Sprintf("/tmp/chip-%v.png", i))
 		png.Encode(outputFile, chip)
-
-		// Don't forget to close files
 		outputFile.Close()
 
 		chips[i] = Chip{
@@ -150,14 +109,7 @@ func main() {
 	}
 
 	for i, chip := range chips {
-		buf := new(bytes.Buffer)
-		err := jpeg.Encode(buf, im, nil)
-
-		// Run inference on *imageFile.
-		// For multiple images, session.Run() can be called in a loop (and
-		// concurrently). Alternatively, images can be batched since the model
-		// accepts batches of image data as input.
-		tensor, err := makeTensorFromImage(fmt.Sprintf("/tmp/chip-%v.png",i))
+		tensor, err := makeTensorFromImage(fmt.Sprintf("/tmp/chip-%v.png", i))
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -192,7 +144,6 @@ func main() {
 					Confidence:  score,
 				})
 		}
-
 		printDetections(detects, labelsfile)
 	}
 }
@@ -235,9 +186,7 @@ func printDetections(detects []Detect, labelsFile string) {
 	}
 
 	for _, d := range detects {
-		//if d.Confidence > .1 {
 		fmt.Printf("%v %v %v %v %v %v\n", d.Bounds.Min.X, d.Bounds.Min.Y, d.Bounds.Max.X, d.Bounds.Max.Y, d.Class, d.Confidence)
-		//}
 	}
 }
 
@@ -273,33 +222,12 @@ func makeTensorFromImage(filename string) (*tf.Tensor, error) {
 	return normalized[0], nil
 }
 
-// The inception model takes as input the image described by a Tensor in a very
-// specific normalized format (a particular image size, shape of the input tensor,
-// normalized pixel values etc.).
-//
-// This function constructs a graph of TensorFlow operations which takes as
-// input a JPEG-encoded string and returns a tensor suitable as input to the
-// inception model.
 func constructGraphToNormalizeImage() (graph *tf.Graph, input, output tf.Output, err error) {
-	// - input is a String-Tensor, where the string the JPEG-encoded image.
-	// - The inception model takes a 4D tensor of shape
-	//   [BatchSize, Height, Width, Colors=3], where each pixel is
-	//   represented as a triplet of floats
-	// - Apply normalization on each pixel and use ExpandDims to make
-	//   this single image be a "batch" of size 1 for ResizeBilinear.
 	s := op.NewScope()
 	input = op.Placeholder(s, tf.String)
 	output = op.ExpandDims(s,
-				op.DecodeJpeg(s, input, op.DecodeJpegChannels(3)),
-				op.Const(s.SubScope("make_batch"), int32(0)))
-
-
-
-	// prechipping to appropriate size
-	//op.Const(s.SubScope("scale"), Scale))
-
-	// https://github.com/tensorflow/models/issues/1741#issuecomment-317501641
-	//output = op.Cast(s.SubScope("final_resize"), output, tf.Uint8)
+		op.DecodeJpeg(s, input, op.DecodeJpegChannels(3)),
+		op.Const(s.SubScope("make_batch"), int32(0)))
 
 	graph, err = s.Finalize()
 	return graph, input, output, err
