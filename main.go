0c13c0205b34e4f37535a2d634009b682a2b10fc
John Wass <wassj@ctc.com>
2020-01-31 14:36:39 -0500

    write chips to filesystem for now

--- main.go
+++ main.go
@@ -2,15 +2,47 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
 	tf "github.com/tensorflow/tensorflow/tensorflow/go"
 	"github.com/tensorflow/tensorflow/tensorflow/go/op"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
+
+// Some constants specific to the pre-trained model at:
+// https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
+//
+// - The model was trained after with images scaled to 224x224 pixels.
+// - The colors, represented as R, G, B in 1-byte each were converted to
+//   float using (value - Mean)/Scale.
+const (
+	H, W  = 300, 300
+	Mean  = float32(117)
+	Scale = float32(1)
+)
+
+type Chip struct {
+	X  int
+	Y  int
+	Im image.Image
+}
+
+type Detect struct {
+	Bounds      image.Rectangle
+	Class       int
+	Chip        *Chip
+	Description string
+	Confidence  float32
+}
 
 func main() {
 	// An example for using the TensorFlow Go API for image recognition
@@ -52,7 +84,7 @@ func main() {
 		return
 	}
 	// Load the serialized GraphDef from a file.
-	modelfile, labelsfile, err := modelFiles(*modeldir, "vanilla")
+	modelfile, labelsfile, err := modelFiles(*modeldir, "multires_aug")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -74,58 +106,139 @@ func main() {
 	}
 	defer session.Close()
 
-	// Run inference on *imageFile.
-	// For multiple images, session.Run() can be called in a loop (and
-	// concurrently). Alternatively, images can be batched since the model
-	// accepts batches of image data as input.
-	tensor, err := makeTensorFromImage(*imagefile)
+	file, err := os.Open(*imagefile)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("%v", err)
 	}
-	output, err := session.Run(
-		map[tf.Output]*tf.Tensor{
-			graph.Operation("image_tensor").Output(0): tensor,
-		},
-		[]tf.Output{
-			graph.Operation("detection_boxes").Output(0),
-			graph.Operation("detection_scores").Output(0),
-			graph.Operation("detection_classes").Output(0),
-			graph.Operation("num_detections").Output(0),
-		},
-		nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// output[0].Value() is a vector containing probabilities of
-	// labels for each image in the "batch". The batch size was 1.
-	// Find the most probably label index.
-	probabilities := output[1].Value().([][]float32)[0]
-	printBestLabel(probabilities, labelsfile)
-}
 
-func printBestLabel(probabilities []float32, labelsFile string) {
-	bestIdx := 0
-	for i, p := range probabilities {
-		if p > probabilities[bestIdx] {
-			bestIdx = i
+	im, err := jpeg.Decode(file)
+	if err != nil {
+		log.Fatalf("%s: %v\n", *imagefile, err)
+	}
+
+	wn := im.Bounds().Dx() / W
+	hn := im.Bounds().Dy() / H
+
+	chips := make([]Chip, wn*hn)
+	for i := 0; i < wn*hn; i++ {
+		x := i % wn
+		y := i / wn
+		w := x * W
+		h := y * H
+
+		chip := im.(interface {
+			SubImage(r image.Rectangle) image.Image
+		}).SubImage(image.Rect(w, h, w+W, h+H))
+
+		outputFile, err := os.Create(fmt.Sprintf("/tmp/chip-%v.png",i))
+		if err != nil {
+			// Handle error
+		}
+
+		// Encode takes a writer interface and an image interface
+		// We pass it the File and the RGBA
+		png.Encode(outputFile, chip)
+
+		// Don't forget to close files
+		outputFile.Close()
+
+		chips[i] = Chip{
+			x,
+			y,
+			chip,
 		}
 	}
-	// Found the best match. Read the string from labelsFile, which
-	// contains one line per label.
+
+	for i, chip := range chips {
+		buf := new(bytes.Buffer)
+		err := jpeg.Encode(buf, im, nil)
+
+		// Run inference on *imageFile.
+		// For multiple images, session.Run() can be called in a loop (and
+		// concurrently). Alternatively, images can be batched since the model
+		// accepts batches of image data as input.
+		tensor, err := makeTensorFromImage(fmt.Sprintf("/tmp/chip-%v.png",i))
+		if err != nil {
+			log.Fatal(err)
+		}
+		output, err := session.Run(
+			map[tf.Output]*tf.Tensor{
+				graph.Operation("image_tensor").Output(0): tensor,
+			},
+			[]tf.Output{
+				graph.Operation("detection_boxes").Output(0),
+				graph.Operation("detection_scores").Output(0),
+				graph.Operation("detection_classes").Output(0),
+				graph.Operation("num_detections").Output(0),
+			},
+			nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		boxes := output[0].Value().([][][]float32)[0]
+		scores := output[1].Value().([][]float32)[0]
+		classes := output[2].Value().([][]float32)[0]
+
+		detects := make([]Detect, 1)
+		for i, score := range scores {
+			class := classes[i]
+			detects = append(detects,
+				Detect{
+					Bounds:      transformBox(chip.X, chip.Y, boxes[i]),
+					Class:       int(class),
+					Chip:        &chip,
+					Description: "",
+					Confidence:  score,
+				})
+		}
+
+		printDetections(detects, labelsfile)
+	}
+}
+
+func transformBox(chipX, chipY int, box []float32) image.Rectangle {
+	//     chip pos   ->  world pos
+	mx := int(box[0]*W) + (chipX * W)
+	Mx := int(box[2]*W) + (chipX * W)
+	my := int(box[1]*H) + (chipY * H)
+	My := int(box[3]*H) + (chipY * H)
+
+	return image.Rectangle{
+		Min: image.Point{
+			X: mx,
+			Y: my,
+		},
+		Max: image.Point{
+			X: Mx,
+			Y: My,
+		},
+	}
+}
+
+func printDetections(detects []Detect, labelsFile string) {
 	file, err := os.Open(labelsFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	var labels []string
-	for scanner.Scan() {
-		labels = append(labels, scanner.Text())
-	}
 	if err := scanner.Err(); err != nil {
 		log.Printf("ERROR: failed to read %s: %v", labelsFile, err)
 	}
-	fmt.Printf("BEST MATCH: (%2.0f%% likely) %s\n", probabilities[bestIdx]*100.0, labels[bestIdx])
+
+	labels := make(map[int]string)
+	for scanner.Scan() {
+		splits := strings.Split(scanner.Text(), ":")
+		id, _ := strconv.Atoi(splits[0])
+		labels[id] = splits[1]
+	}
+
+	for _, d := range detects {
+		//if d.Confidence > .1 {
+		fmt.Printf("%v %v %v %v %v %v\n", d.Bounds.Min.X, d.Bounds.Min.Y, d.Bounds.Max.X, d.Bounds.Max.Y, d.Class, d.Confidence)
+		//}
+	}
 }
 
 // Convert the image in filename to a Tensor suitable as input to the Inception model.
@@ -168,17 +281,6 @@ func makeTensorFromImage(filename string) (*tf.Tensor, error) {
 // input a JPEG-encoded string and returns a tensor suitable as input to the
 // inception model.
 func constructGraphToNormalizeImage() (graph *tf.Graph, input, output tf.Output, err error) {
-	// Some constants specific to the pre-trained model at:
-	// https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
-	//
-	// - The model was trained after with images scaled to 224x224 pixels.
-	// - The colors, represented as R, G, B in 1-byte each were converted to
-	//   float using (value - Mean)/Scale.
-	const (
-		H, W  = 224, 224
-		Mean  = float32(117)
-		Scale = float32(1)
-	)
 	// - input is a String-Tensor, where the string the JPEG-encoded image.
 	// - The inception model takes a 4D tensor of shape
 	//   [BatchSize, Height, Width, Colors=3], where each pixel is
@@ -187,18 +289,17 @@ func constructGraphToNormalizeImage() (graph *tf.Graph, input, output tf.Output,
 	//   this single image be a "batch" of size 1 for ResizeBilinear.
 	s := op.NewScope()
 	input = op.Placeholder(s, tf.String)
-	output = op.Div(s,
-		op.Sub(s,
-			op.ResizeBilinear(s,
-				op.ExpandDims(s,
-					op.Cast(s, op.DecodeJpeg(s, input, op.DecodeJpegChannels(3)), tf.Float),
-					op.Const(s.SubScope("make_batch"), int32(0))),
-				op.Const(s.SubScope("size"), []int32{H, W})),
-			op.Const(s.SubScope("mean"), Mean)),
-		op.Const(s.SubScope("scale"), Scale))
+	output = op.ExpandDims(s,
+				op.DecodeJpeg(s, input, op.DecodeJpegChannels(3)),
+				op.Const(s.SubScope("make_batch"), int32(0)))
+
+
+
+	// prechipping to appropriate size
+	//op.Const(s.SubScope("scale"), Scale))
 
 	// https://github.com/tensorflow/models/issues/1741#issuecomment-317501641
-	output = op.Cast(s.SubScope("final_resize"), output, tf.Uint8)
+	//output = op.Cast(s.SubScope("final_resize"), output, tf.Uint8)
 
 	graph, err = s.Finalize()
 	return graph, input, output, err
