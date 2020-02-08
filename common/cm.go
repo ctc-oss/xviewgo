f3b894c2ccf3c9dff4b858b7722915b10c4171d1
John Wass <wassj@ctc.com>
2020-02-08 01:20:45 -0500

    remove the show matrix func

--- common/cm.go
+++ common/cm.go
@@ -8,7 +8,6 @@ import (
 	"bytes"
 	"fmt"
 	"sort"
-	"strconv"
 	"text/tabwriter"
 )
 
@@ -218,44 +217,6 @@ func GetSummary(c ConfusionMatrix) string {
 	w.Flush()
 	buffer.WriteString(fmt.Sprintf("False Positives:  %v\n", c[0].T))
 	buffer.WriteString(fmt.Sprintf("Overall accuracy: %.4f\n", GetAccuracy(c)))
-
-	return buffer.String()
-}
-
-// ShowConfusionMatrix return a human-readable version of a given
-// ConfusionMatrix.
-func ShowConfusionMatrix(c ConfusionMatrix) string {
-	var buffer bytes.Buffer
-	w := new(tabwriter.Writer)
-	w.Init(&buffer, 0, 8, 0, '\t', 0)
-
-	ref := make([]string, 0)
-	fmt.Fprintf(w, "Reference Class\t")
-	for k := range c {
-		fmt.Fprintf(w, "%s\t", k)
-		ref = append(ref, string(k))
-	}
-	fmt.Fprintf(w, "\n")
-
-	fmt.Fprintf(w, "---------------\t")
-	for _, v := range ref {
-		for t := 0; t < len(v); t++ {
-			fmt.Fprintf(w, "-")
-		}
-		fmt.Fprintf(w, "\t")
-	}
-	fmt.Fprintf(w, "\n")
-
-	for _, v := range ref {
-		fmt.Fprintf(w, "%s\t", v)
-		for _, v2 := range ref {
-			vv, _ := strconv.Atoi(v)
-			vv2, _ := strconv.Atoi(v2)
-			fmt.Fprintf(w, "%d\t", c[CID(vv)].P[CID(vv2)])
-		}
-		fmt.Fprintf(w, "\n")
-	}
-	w.Flush()
 
 	return buffer.String()
 }
